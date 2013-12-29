c012a1913b2076896b62e81dd82c7e60b188a835
lozohcum <lozohcum@gmail.com>
2013-12-29 12:39:43 +0100

    Added missing memtrace module
    Conflicts:
    	common.mk
    
    Change-Id: I3c2fefca7e5abcf7ef10f404d40de9a66cbc9787

--- Android.mk
+++ Android.mk
@@ -1,6 +1,8 @@
 ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
 display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils
+display-hals += libmemtrack
+
 ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
 display-hals += liblight
 endif
